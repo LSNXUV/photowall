3d4efc911f6897593415116516bac6e1cb2ed132
LSuperNova <LSuperNova@qq.com>
2024-02-24 16:34:47 +0800

    添加检测播放策略

--- src/components/Music.tsx
+++ src/components/Music.tsx
@@ -23,6 +23,20 @@ function Music() {
             setIsSetVolume(true)
         }
     }, [ref, isSetVolume])
+
+    //每隔5秒尝试播放
+    useEffect(() => {
+        const interval = setInterval(() => {
+            
+            if (state.paused) {
+                controls.play()
+            }
+            if (state1.paused) {
+                controls1.play()
+            }
+        }, 5000)
+        return () => clearInterval(interval)
+    }, [state, controls, state1, controls1])
     return (
         <div>
             {audio}
